1f1bd4f8d0bb4223f257cc4080a153e93117c6dc
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2019-10-29 00:25:42 -0700

    populate with autumnal colors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://colorhunt.co/palette/132892
+// How do I transition these?
+const colors = [
+  'f3f0d1',
+  'e29c68',
+  'c85108',
+  'a20e0e'
+]
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: `#${colors[0]}`
+    }
+
+  }
+
+  render() {
+    return (
+      <div style={{
+        'minWidth': '100vw',
+        'minHeight': '100vh',
+        'backgroundColor': this.state.color
+      }}>
+      </div>
+    );
+  }
 }
 
 export default App;
