0a02285452fd9cb10da5e686398d30cafb6cb9a5
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2020-11-11 13:25:53 -0800

    feat: include five colors blocks

--- src/App.js
+++ src/App.js
@@ -17,7 +17,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ColorBlock />
+        <ColorBlock width="100vw" height="20vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="20vh"/>
+        <ColorBlock palette="summer" width="100vw" height="20vh"/>
+        <ColorBlock palette="winter" width="100vw" height="20vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="20vh"/>
       </div>
     )
   }
