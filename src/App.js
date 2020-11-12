f69063f78786cf158d6fead5f5ab6e200be96ee3
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2020-11-11 16:34:52 -0800

    feat: subdivide app into 10 blocks

--- src/App.js
+++ src/App.js
@@ -17,10 +17,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ColorBlock palette="spring" width="100vw" height="25vh"/>
-        <ColorBlock palette="autumn" width="100vw" height="25vh"/>
-        <ColorBlock palette="summer" width="100vw" height="25vh"/>
-        <ColorBlock palette="winter" width="100vw" height="25vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
+        <ColorBlock palette="autumn" width="100vw" height="10vh"/>
       </div>
     )
   }
