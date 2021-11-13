088fef7d348b16ddd2387f159d38325130d9b329
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-11-13 05:00:37 +0000

    guess: move bsl into App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import ColorBlock from './components/color-block'
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 
-function App () {
-  return (
-    <div className="app">
-      <ColorBlock palette="autumn" />
-    </div>
-  )
+class App extends React.Component {
+  targetRef = React.createRef()
+  targetElement = null
+
+  componentDidMount () {
+    this.targetElement = this.targetRef.current
+    disableBodyScroll(this.targetElement)
+  }
+
+  componentWillUnmount () {
+    clearAllBodyScrollLocks()
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <ColorBlock
+          ref={this.targetRef}
+          palette="autumn" />
+      </div>
+    )
+  }
 }
 
 export default App
